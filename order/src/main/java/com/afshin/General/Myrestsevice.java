b9b8f1fc3ad10b89cbb951bcf0db957287a6a2d1
a.parhizkari <Afshin.Parhizkari@gmail.com>
2021-02-25 14:29:51 +0330

    Add Restful Webservice on PaymentWs

--- order/src/main/java/com/afshin/General/Myrestsevice.java
+++ order/src/main/java/com/afshin/General/Myrestsevice.java
@@ -1,6 +1,6 @@
-package com.afshin.Webservice;
+package com.afshin.General;
 
-import com.afshin.Entity.Payment;
+import com.afshin.Webservice.PaymentWs;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -8,9 +8,8 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 @ApplicationPath("/rest")
-public class MyApplication extends Application {
-    public Set<Class<?>> MyApplication() {
+public class Myrestsevice extends Application {
+    public Set<Class<?>> Myrestsevice() {
         return new HashSet<Class<?>>(Arrays.asList(PaymentWs.class));
     }
-
 }
